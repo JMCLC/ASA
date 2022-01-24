812229a11bd629c29aab2db279b841c791263d9e
PulgaPT <61068298+PulgaPT@users.noreply.github.com>
2022-01-24 00:33:47 +0000

    Almost finished

--- 2/projeto.cpp
+++ 2/projeto.cpp
@@ -11,63 +11,66 @@
 typedef struct {
     int id;
     bool visited;
+    bool loopCheck;
     std::vector<int> parents;
 } vertex;
 
+bool foundFirstVertex = false;
+bool foundSecondVertex = false;
+
 int initialize_graph(std::vector<vertex> &graph, std::vector<int> &graphInfo) {
-    int currentIndex = 0;
-    int currentId;
     for (int i = 0; i < graphInfo[0]; i++) {
         vertex newVertex = {i + 1, false};
         graph[i] = newVertex;
     }
     std::vector<int> currentConnection(2, -1);
+    int currentParentsSize;
+    int numberOfInput;
     for (int i = 0; i < graphInfo[1]; i++) {
         currentConnection[0] = -1;
         currentConnection[1] = -1;
-        currentIndex = 0;
-        while (std::cin >> currentId) {
-            currentConnection[currentIndex++] = currentId; 
-            if (std::cin.peek() == '\n' || std::cin.peek() == '\0') {
-                break;
-            }
-
-        }
-        if (currentConnection[0] == -1 || currentConnection[1] == -1 || graph[currentConnection[1] - 1].parents.size() == 2) {
+        numberOfInput = scanf("%d %d", &currentConnection[0], &currentConnection[1]);
+        if (numberOfInput < 2) {
             std::cout << 0 << std::endl;
             return 0;
+        }
+        if (currentConnection[0] == -1 || currentConnection[1] == -1 || graph[currentConnection[1] - 1].parents.size() == 2 || currentConnection[0] > graphInfo[0] || currentConnection[1] > graphInfo[0]) {
+            std::cout << 0 << std::endl;
+            return 0;
+        }
+        currentParentsSize = graph[currentConnection[0] - 1].parents.size();
+        for (int i = 0; i < currentParentsSize; i++) {
+            if (graph[currentConnection[0] - 1].parents[i] == currentConnection[1] - 1) {
+                std::cout << 0 << std::endl;
+                return 0;
+            }
         }
         graph[currentConnection[1] - 1].parents.push_back(currentConnection[0] - 1);
     }
     return 1;
 }
 
-void receive_line(std::vector<int> &subjects) {
-    int currentIndex = 0;
-    int currentId;
-    while (std::cin >> currentId) {
-        subjects[currentIndex++] = currentId; 
-        if (std::cin.peek() == '\n' || std::cin.peek() == '\0') {
-            break;
-        }
-    }
-}
-
-void First_DFS(std::vector<vertex> &graph, int currentValue) {
+void First_DFS(std::vector<vertex> &graph, int currentValue, int *secondVertex) {
     int numberOfParents = graph[currentValue].parents.size();
     for (int i = 0; i < numberOfParents; i++) {
         graph[graph[currentValue].parents[i]].visited = true;
-        First_DFS(graph, graph[currentValue].parents[i]);
+        if (graph[graph[currentValue].parents[i]].id == *secondVertex) {
+            foundSecondVertex = true;
+            return;
+        }
+        First_DFS(graph, graph[currentValue].parents[i], secondVertex);
     }
 }
 
-void Second_DFS(std::vector<vertex> &graph, std::vector<int> &answers, int currentValue) {
+void Second_DFS(std::vector<vertex> &graph, std::vector<int> &answers, int currentValue, int *firstVertex) {
     int numberOfParents = graph[currentValue].parents.size();
     for (int i = 0; i < numberOfParents; i++) {
+        if (graph[graph[currentValue].parents[i]].id == *firstVertex)
+            foundFirstVertex = true;
         if (graph[graph[currentValue].parents[i]].visited == true)
             answers[graph[currentValue].parents[i]] = 1;
         else
-            Second_DFS(graph, answers, graph[currentValue].parents[i]);
+            Second_DFS(graph, answers, graph[currentValue].parents[i], firstVertex);
     }
 }
 
@@ -80,13 +83,46 @@ void Third_DFS(std::vector<vertex> &graph, std::vector<int> &answers, int curren
     }
 }
 
+// int loopCheck_aux(std::vector<vertex> &graph, int currentValue) {
+//     int numberOfParents = graph[currentValue].parents.size();
+//     for (int i = 0; i < numberOfParents; i++) {
+//         graph[graph[currentValue].parents[i]].loopCheck = true;
+//         if (graph[graph[currentValue].parents[i]].) {
+//             foundSecondVertex = true;
+//             return;
+//         }
+//         First_DFS(graph, graph[currentValue].parents[i]);
+//     }
+// }
+
+int loopCheck(std::vector<vertex> &graph) {
+    for (vertex v: graph) {
+        if (v.loopCheck == false) {
+            // loopCheck_aux(graph, v.id - 1);
+            int numberOfParents = v.parents.size();
+            for (int i = 0; i < numberOfParents; i++) {
+                graph[v.parents[i]].loopCheck = true;                
+            }
+        }
+    }
+    return 1;
+}
+
 void exercise(std::vector<vertex> &graph, std::vector<int> &subjects, int *size) {
     std::vector<int> answers(*size, 0);
     graph[subjects[0] - 1].visited = 1;
-    First_DFS(graph, subjects[0] - 1);
+    First_DFS(graph, subjects[0] - 1, &subjects[1]);
+    if (foundSecondVertex == true) {
+        std::cout << subjects[1] << " " << std::endl;
+        return;
+    }
     if (graph[subjects[1] - 1].visited == 1)
         answers[subjects[1] - 1] = 1;
-    Second_DFS(graph, answers, subjects[1] - 1);
+    Second_DFS(graph, answers, subjects[1] - 1, &subjects[0]);
+    if (foundSecondVertex == true) {
+        std::cout << subjects[0] << " " << std::endl;
+        return;
+    }
     for (int i = 0; i < *size; i++) {
         if (answers[i] == 1)
             Third_DFS(graph, answers, i);
@@ -109,11 +145,22 @@ void exercise(std::vector<vertex> &graph, std::vector<int> &subjects, int *size)
 int main() {
     std::vector<int> subjects(2, -1);
     std::vector<int> graphInfo(2, -1);
-    receive_line(subjects);
-    receive_line(graphInfo);
+    int numberOfSubjects = scanf("%d %d", &subjects[0], &subjects[1]);
+    if (numberOfSubjects < 2) {
+        std::cout << 0 << std::endl;
+        return 0;
+    }
+    int numberOfGraphInfo = scanf("%d %d", &graphInfo[0], &graphInfo[1]);
+    if (numberOfGraphInfo < 2) {
+        std::cout << 0 << std::endl;
+        return 0;
+    }
     std::vector<vertex> graph(graphInfo[0], {-1});
     int initialized = initialize_graph(graph, graphInfo);
-    if (initialized == 1)
-        exercise(graph, subjects, &graphInfo[0]);
+    if (initialized == 1) {
+        int loopExistence = loopCheck();
+        if (loopExistence == 1)
+            exercise(graph, subjects, &graphInfo[0]);
+    }
     return 0;
 }
